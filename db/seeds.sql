2af4d50faaa8ed8f00da189b021f817c3218e1fe
Orvin Welchez <owelchez@gmail.com>
2016-08-14 12:33:09 -0400

    Init

---
+++ db/seeds.sql
@@ -0,0 +1,11 @@
+INSERT INTO burgers (burger_name, devour)
+VALUES ('Whopper', false);
+
+INSERT INTO burgers (burger_name, devour)
+VALUES ('krabby patty', false);
+
+INSERT INTO burgers (burger_name, devour)
+VALUES ('Matador Burger', false);
+
+INSERT INTO burgers (burger_name, devour)
+VALUES ('Bare Burger', false);
