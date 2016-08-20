afdf123dc0ecdc58be2fcf136075ec37f74200b3
Orvin Welchez <owelchez@gmail.com>
2016-08-20 03:15:20 -0400

    Update

--- server.js
+++ server.js
@@ -3,16 +3,18 @@ var bodyParser = require('body-parser');
 var mysql = require('mysql');
 var exphbs = require('express-handlebars');
 var methodOverride = require('method-override');
+var models = require('./models')
+models.sequelize.sync()
 
 var app = express(); // DUH!
 
 /*MySQL connection initialization*/
-var connection = mysql.createConnection({
+/*var connection = mysql.createConnection({
   host     : 'localhost',
   user     : 'root',
   password : '',
   database : 'burgers_db'
-});
+});*/
 
 // parse application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: false }))
@@ -24,13 +26,22 @@ app.set('view engine', 'handlebars');
 //Serve static content for the app from the "public" directory in the application directory.
 app.use(express.static(__dirname + '/public'));
 
-connection.connect(function (err) {
+/*connection.connect(function (err) {
   if (err) {
     console.error('error connecting: ' + err.stack);
     return;
   }
   console.log('connected as id ' + connection.threadId);
-});
+});*/
+
+
+
+
+
+
+
+
+
 
 
 /********************************************/
@@ -51,7 +62,7 @@ connection.connect(function (err) {
                     /*^^^^^^ This is working! ^^^^^^*/
 
                   /*Root route for devoured burgers*/
-app.get('/devoured', function(req, res){
+/*app.get('/devoured', function(req, res){
   connection.query('SELECT * FROM burgers WHERE devoured = true', function(err, data){
     if(err){
       throw err;
@@ -59,13 +70,13 @@ app.get('/devoured', function(req, res){
       res.render('index', {devoured: data});
     }
   })
-})
+})*/
 
             /*^^^^^^ Working on this! ^^^^^^*/
 
 
                       /*POST route*/
-app.post('/create', function(req, res){
+/*app.post('/create', function(req, res){
   connection.query('INSERT INTO burgers (burger_name, devoured) VALUES (?, false)', [req.body.burger_name, req.body.devoured], function(err, data){
     if(err){
       throw err;
@@ -73,11 +84,11 @@ app.post('/create', function(req, res){
       res.redirect('/');
     }
   });
-});
+});*/
 
                     /*^^^^^^ This is working ^^^^^^*/
 
-app.delete('/delete', function(req, res){
+/*app.delete('/delete', function(req, res){
   connection.query('DELETE FROM burgers WHERE id = ?', [req.body.id], function(err, result){
     if(err){
       throw err;
@@ -85,7 +96,7 @@ app.delete('/delete', function(req, res){
       res.redirect('/');
     }
   });
-});
+});*/
 
                       /*^^^^^^ This is working ^^^^^^*/
 
